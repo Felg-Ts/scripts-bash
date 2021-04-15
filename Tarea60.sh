cf3467acd20e846116c23f46818a9562b996ebc4
Felg-Ts <el-feli-rubio1996@hotmail.com>
2021-04-15 18:09:03 +0200

    nuevas funciones

--- Tarea60.sh
+++ Tarea60.sh
@@ -5,15 +5,32 @@ blkid | egrep /dev/$d
 }
 
 f_modifica_fstab () {
-
+lsblk
+echo "Indique el nombre del dispositivo de bloques que quiere habilitar para quota"
 }
 
 f_habilita_quota () {
-
+echo "Buscando ficheros de cuotas"
+if
+find / aquota | egrep -o aquota 2> /dev/nulls
+then
+echo "ya existen los ficheros"
+else
+echo "Creando ficheros de coutas"
+quotacheck -ugm /QUOTA
+find / aquota | egrep -o aquota 2> /dev/nulls
+quotaon -V /QUOTA
+ficheros
 }
 
 f_plantilla_quota () {
-
+echo "Donde desea realizar la configuración 'usuario' o 'grupo'"
+read var1
+if var1 == "usuario"
+echo"Usuario selecccionado"
+elif var1 == "grupo"
+echo"grupo selecccionado"
+fi
 }
 
 f_configura_quota () {
